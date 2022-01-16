793a2ea9ed541a8b1504592996eeb77f84c80598
Christopher Jenkins <christj@gmail.com>
2022-01-16 07:56:13 -0700

    mock class method rather than whole method under test

--- spec/getv_spec.rb
+++ spec/getv_spec.rb
@@ -7,13 +7,19 @@ RSpec.describe Getv do
 end
 
 RSpec.describe Getv::Package do
+  let(:expected_versions) do
+    ['0.9.0', '1.0.0']
+  end
+
   context '::docker' do
     it 'returns correct docker version' do
       @package = described_class.new 'mypackage', type: 'docker', owner: 'apache', reject: '-'
-      allow(@package).to receive(:versions_using_docker).and_return(['0.9.0', '1.0.0'])
+      # allow(@package).to receive(:versions_using_docker).and_return(['0.9.0', '1.0.0'])
+      require 'docker_registry2'
+      allow(DockerRegistry2).to receive(:connect).and_return(double(tags: { 'tags' => expected_versions }))
       @package.versions
-      expect(@package.opts[:latest_version]).to eq '1.0.0'
-      expect(@package.opts[:versions]).to eq ['0.9.0', '1.0.0']
+      expect(@package.opts[:latest_version]).to eq expected_versions.last
+      expect(@package.opts[:versions]).to eq expected_versions
     end
   end
 end
