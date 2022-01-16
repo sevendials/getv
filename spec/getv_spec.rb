4a5afb8b5182e4a9ff27e58b5f1cc334072a66ab
Christopher Jenkins <christj@gmail.com>
2022-01-16 10:07:41 -0700

    split out tests

--- spec/getv_spec.rb
+++ spec/getv_spec.rb
@@ -8,11 +8,25 @@ end
 
 RSpec.describe Getv::Package, :vcr do
   context '::docker' do
-    it 'returns correct docker version' do
+    let :expected_versions do
+      ['1.0.0', '1.0.1', '1.1.0', '1.2.0', '1.3.0', '1.3.1', '1.3.2']
+    end
+    before(:each) do
       @package = described_class.new 'superset', type: 'docker', owner: 'apache', reject: '-'
       @package.versions
-      expect(@package.opts[:latest_version]).to eq '1.3.2'
-      expect(@package.opts[:versions]).to eq ['1.0.0', '1.0.1', '1.1.0', '1.2.0', '1.3.0', '1.3.1', '1.3.2']
+    end
+
+    after(:each) do
+      @package.opts.delete :versions
+      @package.opts.delete :latest_version
+    end
+
+    it 'returns correct docker versions' do
+      expect(@package.opts[:versions]).to eq expected_versions
+    end
+
+    it 'returns latest version' do
+      expect(@package.opts[:latest_version]).to eq expected_versions.last
     end
   end
 end
