f5296357da8ea44f53a9d6b48d544a0dede87111
Christopher Jenkins <christj@gmail.com>
2022-01-16 10:37:05 -0700

    add gem test

--- spec/getv/package_spec.rb
+++ spec/getv/package_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe Getv::Package, :vcr do
       expect(@package.opts[:latest_version]).to eq expected_versions.last
     end
   end
+
+  context '::gem' do
+    let :expected_versions do
+      ['7.12.0', '7.12.0', '7.12.1', '7.12.1', '7.12.1', '7.12.1', '7.13.1', '7.13.1', '7.13.1', '7.13.1']
+    end
+    before(:each) do
+      @package = described_class.new 'puppet', type: 'gem', reject: '-'
+      @package.versions
+    end
+
+    after(:each) do
+      @package.opts.delete :versions
+      @package.opts.delete :latest_version
+    end
+
+    it 'returns correct versions' do
+      expect(@package.opts[:versions][-10..]).to eq expected_versions
+    end
+
+    it 'returns latest version' do
+      expect(@package.opts[:latest_version]).to eq expected_versions.last
+    end
+  end
 end
